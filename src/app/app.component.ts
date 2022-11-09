9294414df620198d3a99dd585a58e9a1694524e8
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2022-11-09 22:32:54 +0100

    Implement editing employee

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import {NgForm} from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   public employees: Employee[] | undefined;
+  public editEmployee: Employee | undefined;
 
   constructor(private employeeService: EmployeeService) {
   }
@@ -43,6 +44,18 @@ export class AppComponent implements OnInit {
     );
   }
 
+  public onUpdateEmployee(employee: Employee): void {
+    this.employeeService.updateEmployee(employee).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
   public onOpenModal(employee: Employee | null, mode: string): void {
     const container = document.getElementById("main-container")
     const button = document.createElement('button')
@@ -53,6 +66,7 @@ export class AppComponent implements OnInit {
       button.setAttribute("data-target", "#addEmployeeModal");
     }
     if (mode === 'edit') {
+      this.editEmployee = employee!;
       button.setAttribute("data-target", "#updateEmployeeModal");
     }
     if (mode === 'delete') {
